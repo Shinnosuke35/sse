52abc2dd2f8357c236fc41221cbb0c9a00a589dc
Shinnosuke35 <tetu0503Shin@icloud.com>
2021-04-19 17:23:18 +0900

    Initial Commit

---
+++ sse/sseApp.swift
@@ -0,0 +1,17 @@
+//
+//  sseApp.swift
+//  sse
+//
+//  Created by 手塚晋之介 on 2021/04/19.
+//
+
+import SwiftUI
+
+@main
+struct sseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
